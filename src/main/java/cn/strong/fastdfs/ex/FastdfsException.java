1c00e299f3b053b61361b31b60971748d9f13c66
liulongbiao <liulongbiao@cnstrong.cn>
2016-07-14 16:46:19 +0800

    add exception

---
+++ src/main/java/cn/strong/fastdfs/ex/FastdfsException.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package cn.strong.fastdfs.ex;
+
+/**
+ * Fastdfs 异常
+ * 
+ * @author liulongbiao
+ *
+ */
+public class FastdfsException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8274679139300220262L;
+
+	public FastdfsException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public FastdfsException(String message) {
+		super(message);
+	}
+
+	public FastdfsException(Throwable cause) {
+		super(cause);
+	}
+
+}
