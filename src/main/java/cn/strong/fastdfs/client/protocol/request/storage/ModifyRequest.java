44eaeaac095b8228785cb42dc9a19cbe6c61e553
liulongbiao <liulongbiao@cnstrong.cn>
2016-07-15 10:46:43 +0800

    add Modify

---
+++ src/main/java/cn/strong/fastdfs/client/protocol/request/storage/ModifyRequest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package cn.strong.fastdfs.client.protocol.request.storage;
+
+import static io.netty.util.CharsetUtil.UTF_8;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+
+import java.io.File;
+
+import cn.strong.fastdfs.client.CommandCodes;
+import cn.strong.fastdfs.client.Consts;
+import cn.strong.fastdfs.model.StoragePath;
+
+/**
+ * 修改文件请求
+ * 
+ * @author liulongbiao
+ *
+ */
+public class ModifyRequest extends AbstractFileRequest {
+
+	public final StoragePath spath;
+	public final int offset;
+
+	public ModifyRequest(File file, StoragePath spath, int offset) {
+		super(file);
+		this.spath = spath;
+		this.offset = offset;
+	}
+
+	public ModifyRequest(Object content, long size, StoragePath spath, int offset) {
+		super(content, size);
+		this.spath = spath;
+		this.offset = offset;
+	}
+
+	@Override
+	protected byte cmd() {
+		return CommandCodes.STORAGE_PROTO_CMD_MODIFY_FILE;
+	}
+
+	@Override
+	protected ByteBuf meta(ByteBufAllocator alloc) {
+		byte[] pathBytes = spath.path.getBytes(UTF_8);
+		int metaLen = 3 * Consts.FDFS_PROTO_PKG_LEN_SIZE + pathBytes.length;
+		ByteBuf buf = alloc.buffer(metaLen);
+		buf.writeLong(pathBytes.length);
+		buf.writeLong(offset);
+		buf.writeLong(size);
+		buf.writeBytes(pathBytes);
+		return buf;
+	}
+
+}
