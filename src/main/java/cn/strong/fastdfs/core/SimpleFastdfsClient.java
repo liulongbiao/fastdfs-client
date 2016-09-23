5fb4ad3c6bdf9cec6d64db474234e75c9a1dea77
liulongbiao <liulongbiao@cnstrong.cn>
2016-09-23 09:56:31 +0800

    update SimpleFastdfsClient use ComplatableFuture

--- src/main/java/cn/strong/fastdfs/core/SimpleFastdfsClient.java
+++ src/main/java/cn/strong/fastdfs/core/SimpleFastdfsClient.java
@@ -7,6 +7,7 @@ import static cn.strong.fastdfs.model.StoragePath.fromFullPath;
 
 import java.io.File;
 import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
 
 import cn.strong.fastdfs.client.Consts;
 import cn.strong.fastdfs.client.FastdfsTemplate;
@@ -17,12 +18,11 @@ import cn.strong.fastdfs.sink.ByteArraySink;
 import cn.strong.fastdfs.sink.FileSink;
 import cn.strong.fastdfs.sink.SinkProgressListener;
 import cn.strong.fastdfs.utils.Seed;
-import rx.Observable;
 
 /**
  * 简单 FastDFS 客户端
  * <ul>
- * <li>使用同步方法，降低未学过 RxJava 的同学的学习曲线</li>
+ * <li>使用同步方法，降低未学过 CompletableFuture 的同学的学习曲线</li>
  * <li>存储路径使用全路径</li>
  * </ul>
  * 
@@ -46,11 +46,11 @@ public class SimpleFastdfsClient {
 	 * @param observable
 	 * @return
 	 */
-	private static <T> T await(Observable<T> observable) {
+	private static <T> T await(CompletableFuture<T> f) {
 		try {
-			return observable.toBlocking().single();
+			return f.get();
 		} catch (Exception e) {
-			throw new FastdfsException("get single result from observable error", e);
+			throw new FastdfsException("get result from CompletableFuture error", e);
 		}
 	}
 
@@ -79,7 +79,7 @@ public class SimpleFastdfsClient {
 	 * @return 服务器存储路径
 	 */
 	public String upload(byte[] bytes, String ext, String group) {
-		return await(delegate.upload(bytes, bytes.length, ext, group).map(StoragePath::getFullPath));
+		return await(delegate.upload(bytes, bytes.length, ext, group).thenApply(StoragePath::getFullPath));
 	}
 
 	/**
@@ -106,7 +106,7 @@ public class SimpleFastdfsClient {
 		if (file == null || !file.exists()) {
 			throw new FastdfsException("file does not exist.");
 		}
-		return await(delegate.upload(file, group).map(StoragePath::getFullPath));
+		return await(delegate.upload(file, group).thenApply(StoragePath::getFullPath));
 	}
 
 	/**
@@ -133,7 +133,7 @@ public class SimpleFastdfsClient {
 	 * @return 服务器存储路径
 	 */
 	public String upload(Object content, long size, String ext, String group) {
-		return await(delegate.upload(content, size, ext, group).map(StoragePath::getFullPath));
+		return await(delegate.upload(content, size, ext, group).thenApply(StoragePath::getFullPath));
 	}
 
 	/**
@@ -161,7 +161,7 @@ public class SimpleFastdfsClient {
 	 * @return 服务器存储路径
 	 */
 	public String uploadAppender(byte[] bytes, String ext, String group) {
-		return await(delegate.uploadAppender(bytes, bytes.length, ext, group).map(
+		return await(delegate.uploadAppender(bytes, bytes.length, ext, group).thenApply(
 				StoragePath::getFullPath));
 	}
 
@@ -189,7 +189,7 @@ public class SimpleFastdfsClient {
 		if (file == null || !file.exists()) {
 			throw new FastdfsException("file does not exist.");
 		}
-		return await(delegate.uploadAppender(file, group).map(StoragePath::getFullPath));
+		return await(delegate.uploadAppender(file, group).thenApply(StoragePath::getFullPath));
 	}
 
 	/**
@@ -216,7 +216,7 @@ public class SimpleFastdfsClient {
 	 * @return 服务器存储路径
 	 */
 	public String uploadAppender(Object content, long size, String ext, String group) {
-		return await(delegate.upload(content, size, ext, group).map(StoragePath::getFullPath));
+		return await(delegate.upload(content, size, ext, group).thenApply(StoragePath::getFullPath));
 	}
 
 	/**
@@ -241,7 +241,7 @@ public class SimpleFastdfsClient {
 	 */
 	public byte[] download(String path, SinkProgressListener listener) {
 		try (ByteArraySink sink = new ByteArraySink()) {
-			delegate.download(fromFullPath(path), sink, listener).toCompletable().await();
+			delegate.download(fromFullPath(path), sink, listener).get();
 			return sink.getBytes();
 		} catch (FastdfsException e) {
 			throw e;
@@ -274,7 +274,7 @@ public class SimpleFastdfsClient {
 	 */
 	public void download(String path, File file, SinkProgressListener listener) {
 		try (FileSink sink = new FileSink(file)) {
-			delegate.download(fromFullPath(path), sink, listener).toCompletable().await();
+			delegate.download(fromFullPath(path), sink, listener).get();
 		} catch (FastdfsException e) {
 			throw e;
 		} catch (Exception ex) {
